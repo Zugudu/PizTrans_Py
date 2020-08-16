d773a9244fa3595bec047f8ec495414b69fdc5eb
Adam@Win <Zuq>
2020-08-16 08:39:36 +0300

    401

--- main.py
+++ main.py
@@ -71,54 +71,58 @@ def manga(id):
 	cursor = db.cursor()
 	cursor.execute('select name,dir from hentai where id=?;', (id,))
 	res = cursor.fetchone()
-	content = ''
-	if ADMIN_ON:
-		content = pages.admin_header
+	if res is not None:
+		content = ''
+		if ADMIN_ON:
+			content = pages.admin_header
+			if request.get_cookie('admin') == ADMIN_KEY:
+				content += pages.admin_yes.format(choice(listdir('static/admin')))
+		content = concat(content, '<div class=name>', res[0],
+							'</div><div><div class=block><a href=/show/',
+							id, '><img class=image src="/hentai/',
+							res[1], '/', listdir('hentai/' + res[1])[0],
+							'"></a></div><div class=disc>')
+		genres = cursor.execute('select id_genres from hentai_genres'
+		' where id_hentai=?;', (id,)).fetchall()
+		for genre in genres:
+			cursor.execute('select id,name from genres where id=?;', (genre[0],))
+			genre_info = cursor.fetchone()
+			content = concat(content, '<a id=genre href=/genres/',
+				genre_info[0], '>', genre_info[1], '</a> ')
+
 		if request.get_cookie('admin') == ADMIN_KEY:
-			content += pages.admin_yes.format(choice(listdir('static/admin')))
-	content = concat(content, '<div class=name>', res[0],
-						'</div><div><div class=block><a href=/show/',
-						id, '><img class=image src="/hentai/',
-						res[1], '/', listdir('hentai/' + res[1])[0],
-						'"></a></div><div class=disc>')
-	genres = cursor.execute('select id_genres from hentai_genres'
-	' where id_hentai=?;', (id,)).fetchall()
-	for genre in genres:
-		cursor.execute('select id,name from genres where id=?;', (genre[0],))
-		genre_info = cursor.fetchone()
-		content = concat(content, '<a id=genre href=/genres/',
-			genre_info[0], '>', genre_info[1], '</a> ')
+			content += '<div class="w3-row" style="width:480px;">' \
+				'<div class="w3-half">' \
+				'<form class="w3-container" method="post" action="/a_add">' \
+				'<input type="hidden" name=id value={}>'\
+				'<select multiple size=15 name="genres">'.format(id)
 
-	if request.get_cookie('admin') == ADMIN_KEY:
-		content += '<div class="w3-row" style="width:480px;">' \
-			'<div class="w3-half">' \
-			'<form class="w3-container" method="post" action="/a_add">' \
-			'<input type="hidden" name=id value={}>'\
-			'<select multiple size=15 name="genres">'.format(id)
+			genres_full = cursor.execute('select id, name from genres;').fetchall()
+			genres_exclude = [i for i in genres_full if (i[0],) not in genres]
+			genres_x = [i for i in genres_full if (i[0],) in genres]
 
-		genres_full = cursor.execute('select id, name from genres;').fetchall()
-		genres_exclude = [i for i in genres_full if (i[0],) not in genres]
-		genres_x = [i for i in genres_full if (i[0],) in genres]
+			for genre_i in genres_exclude:
+				content += '<option value="{}">{}</option>'.format(genre_i[0], genre_i[1])
+			content += '</select><br><button class="w3-button '\
+				'w3-dark-gray" type="submit">Додати</button></form></div>'\
+				'<div class="w3-half">'\
+				'<form class="w3-container" method="post" action="/a_del">'\
+				'<input type="hidden" name=id value={}>'\
+				'<select multiple size=15 name="genres">'.format(id)
 
-		for genre_i in genres_exclude:
-			content += '<option value="{}">{}</option>'.format(genre_i[0], genre_i[1])
-		content += '</select><br><button class="w3-button '\
-			'w3-dark-gray" type="submit">Додати</button></form></div>'\
-			'<div class="w3-half">'\
-			'<form class="w3-container" method="post" action="/a_del">'\
-			'<input type="hidden" name=id value={}>'\
-			'<select multiple size=15 name="genres">'.format(id)
+			for genre_i in genres_x:
+				content += '<option value="{}">{}</option>'.format(genre_i[0], genre_i[1])
 
-		for genre_i in genres_x:
-			content += '<option value="{}">{}</option>'.format(genre_i[0], genre_i[1])
-
-		content += '</select><br><button class="w3-button w3-dark-gray"'\
-			' type="submit">Видалити</button></form></div></div>'
-	content += '</div><div><a href=/><img class=control src=/static/ico/la.png>' \
-		'</a> <a href=/show/{}><img class=control src=/static/ico/ra.png>' \
-		'</a></div>'.format(id)
-	cursor.close()
-	return prepare_str(pages.manga, content)
+			content += '</select><br><button class="w3-button w3-dark-gray"'\
+				' type="submit">Видалити</button></form></div></div>'
+		content += '</div><div><a href=/><img class=control src=/static/ico/la.png>' \
+			'</a> <a href=/show/{}><img class=control src=/static/ico/ra.png>' \
+			'</a></div>'.format(id)
+		cursor.close()
+		return prepare_str(pages.manga, content)
+	else:
+		cursor.close()
+		abort(500)
 
 
 @route('/genres/<id:int>')
@@ -219,15 +223,20 @@ def admin_del():
 
 
 @error(404)
-def err404(error):
+def err404(err):
 	return prepare_str(pages.error, 'Як ти сюди потрапив?', '404.png')
 
 
 @error(500)
-def err500(error):
+def err500(err):
 	return prepare_str(pages.error, 'Або ти лізеж куди не треба, або у нас '
-		'полетіла БД. І якщо це так то пізно срати. '
+		'полетіла БД. І якщо це так — то пізно срати. '
 		'Може полагодим скоро, може ні', '500.png')
+
+
+@error(401)
+def err401(err):
+	return prepare_str(pages.error, 'У тебе немає доступу до цього. Йди-но звідси доки не втрапив у халепу', '401.png')
 
 
 if __name__ == '__main__':
