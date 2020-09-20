5fe56646d4748083989842253e1bc79e86f6ec5e
Adam@Win <Zuq>
2020-09-21 00:25:49 +0300

    description

--- pages.py
+++ pages.py
@@ -8,10 +8,10 @@ main_page = '''
 <meta charset=utf8>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name=keywords content="хєнтай хентай манга манґа мангу манґу hentai manga порно комикси комікси українською читати онлайн юхентай uhentai">
+<meta name="description" content="Сайт присвячений перекладу манґи та хентаю українською, командою Юманго"> 
 <link rel="stylesheet" href="/static/css/w3.css">
 <link rel="stylesheet" href="/static/css/style.css" />
-<link rel="stylesheet" media="(max-device-width:480px)" 
-href="/static/css/mob.css"/>
+<link rel="stylesheet" media="(max-device-width:480px)" href="/static/css/mob.css"/>
 <link rel="icon" type="image/png" href="/static/ico/logo.png">
 <title>uHentai</title>
 </head>
